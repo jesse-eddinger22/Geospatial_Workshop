74ef569c11e89ef8ab6c81a23db0a25e8588c8b8
Jesse Eddinger <jesseeddinger22@gmail.com>
2022-08-18 14:21:16 -0500

    plots plots plots

--- scripts/08-vector-raster_Integration.R
+++ scripts/08-vector-raster_Integration.R
@@ -21,14 +21,36 @@ point_HARV <- st_read('data/GeospatialWorkshopData/vector/HARVtower_UTM18N.shp')
 plot_locations_HARV <- read.csv("data/GeospatialWorkshopData/HARV_PlotLocations.csv")
 str(plot_locations_HARV)
 
+plot_locations_HARV$utmZone
 
+# same utm as the harvard data
+st_crs(point_HARV)$proj4string
 
+# Save projection
+utm18n <- st_crs(point_HARV)
+class(utm18n)
 
+# Convert data frame to spatial object
+plot_locations_sp_HARV <- st_as_sf(plot_locations_HARV,
+                                   coords = c('easting','northing'),
+                                   crs = utm18n)
+class(plot_locations_sp_HARV)
+class(point_HARV)
+# They match up
 
+# plot our plot locations, add aoi boundary
+ggplot() +
+  geom_sf(data=aoi_boundary_HARV) +
+  geom_sf(data=plot_locations_sp_HARV) +
+  ggtitle('Map of Plot Locations')
 
-
-
-
+# add some context though 
+ggplot() +
+  geom_sf(data=aoi_boundary_HARV) +
+  geom_sf(data = lines_HARV) +
+  geom_sf(data = point_HARV, shape = 15) +
+  geom_sf(data=plot_locations_sp_HARV) +
+  ggtitle('Map of Plot Locations')
 
 
 
