2d4c92bd2bd9d21cdf3c233e2c93317bdb1fbb45
Jesse Eddinger <jesseeddinger22@gmail.com>
2022-08-18 09:57:07 -0500

    Adding code

--- scripts/07-WorkshopDay02.R
+++ scripts/07-WorkshopDay02.R
@@ -31,8 +31,37 @@ ggplot() +
   ggtitle('AOI Boundary Plot') + 
   coord_sf()
 
+# Challenge 
+# Load in the roads and tower shp files
+lines_HARV <- st_read('data/GeospatialWorkshopData/vector/HARV_roads.shp')
+point_HARV <- st_read('data/GeospatialWorkshopData/vector/HARVtower_UTM18N.shp')
 
+# what type is this object
+st_geometry_type(lines_HARV)
+str(lines_HARV)
 
+st_geometry_type(point_HARV)
+str(point_HARV)
 
+# Let's look at what the crs is 
+st_crs(lines_HARV)$proj4string
+st_crs(point_HARV)$proj4string
 
+# What is the extent
+st_bbox(lines_HARV)
+st_bbox(point_HARV)
+
+# Plot the shape of the field site for lines
+ggplot() +
+  geom_sf(data = lines_HARV, size=3, color='black', 
+          fill='cyan1') +
+  ggtitle('Lines Boundary Plot') + 
+  coord_sf()
+
+# Plot for Point
+ggplot() +
+  geom_sf(data = point_HARV, size=3, color='black', 
+          fill='cyan1') +
+  ggtitle('Point Boundary Plot') + 
+  coord_sf()
 
