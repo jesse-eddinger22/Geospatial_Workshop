00d57482289f23e9d9d9c1fe9e065ba6ff87430f
Jesse Eddinger <jesseeddinger22@gmail.com>
2022-08-18 09:57:07 -0500

    Adding code

--- scripts/07-WorkshopDay02.R
+++ scripts/07-WorkshopDay02.R
@@ -15,6 +15,11 @@ library(gridExtra) # This was just for comparing plots
 # Read in the polygon of our field site
 aoi_boundary_HARV <- st_read('data/GeospatialWorkshopData/vector/HarClip_UTMZ18.shp')
 
+str(aoi_boundary_HARV)
+ncol(aoi_boundary_HARV) # 2 attributes
+names(aoi_boundary_HARV)
+head(aoi_boundary_HARV)
+
 # what type is this object
 st_geometry_type(aoi_boundary_HARV)
 
@@ -31,8 +36,61 @@ ggplot() +
   ggtitle('AOI Boundary Plot') + 
   coord_sf()
 
+# Challenge 
+# Load in the roads and tower shp files
+lines_HARV <- st_read('data/GeospatialWorkshopData/vector/HARV_roads.shp')
+point_HARV <- st_read('data/GeospatialWorkshopData/vector/HARVtower_UTM18N.shp')
 
+# what type is this object
+st_geometry_type(lines_HARV)
+str(lines_HARV)
+ncol(lines_HARV)
+names(lines_HARV)
+head(lines_HARV)
 
+# type for point data
+st_geometry_type(point_HARV) 
+str(point_HARV)
+ncol(point_HARV) # 15 attributes
+names(point_HARV)# Owned by Harvard, Country is not part of this data set
 
+# check ownership in a different way
+point_HARV$Ownership
 
+# Let's look at what the crs is 
+st_crs(lines_HARV)$proj4string
+st_crs(point_HARV)$proj4string
 
+# What is the extent
+st_bbox(lines_HARV)
+st_bbox(point_HARV)
+
+# Plot the shape of the field site for lines
+ggplot() +
+  geom_sf(data = lines_HARV, size=3, color='black', 
+          fill='cyan1') +
+  ggtitle('Lines Boundary Plot') + 
+  coord_sf()
+
+# Plot for Point
+ggplot() +
+  geom_sf(data = point_HARV, size=3, color='black', 
+          fill='cyan1') +
+  ggtitle('Point Boundary Plot') + 
+  coord_sf()
+
+# Filter by type
+lines_HARV$TYPE
+
+# Let's plot only footpaths
+lines_HARV %>% 
+  filter(TYPE == 'footpath') %>% 
+  nrow
+
+# Plot
+lines_HARV %>% 
+  filter(TYPE == 'footpath') %>% 
+  ggplot() +
+  geom_sf(aes(color = factor(OBJECTID)), size = 1.5) +
+  ggtitle('NEON Harvard Forest Field Site', subtitle = 'Footpaths') +
+  coord_sf()
